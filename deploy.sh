1fcdec683455ff9eb09e98cc05f6e8851c05f3b3
박단우 <danwoo.park@sk.com>
2021-10-02 05:59:27 +0900

    Deploy manual script

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,29 @@
 #!/usr/bin/env bash
-ENV_FILE=.env
 REMOTE_SERVER=lightsail
 APP_NAME=afreeca-scraper
-PROJECT_DIR=~/project/${APP_NAME}
+PROJECT_DIR=project/${APP_NAME}
 DOCKER_COMPOSE_FILE_PATH=${PROJECT_DIR}/${APP_NAME}/docker-compose.yml
+
+ENV_FILE=${PROJECT_DIR}/.env
+
 # shellcheck disable=SC2029
 ssh ${REMOTE_SERVER} "docker-compose -f ${DOCKER_COMPOSE_FILE_PATH} down"
 # shellcheck disable=SC2029
 ssh ${REMOTE_SERVER} "rm -rf ${PROJECT_DIR}"
-scp -r . ${REMOTE_SERVER}:${PROJECT_DIR}
+# shellcheck disable=SC2029
+rsync -av ./ lightsail:${PROJECT_DIR}
+
 # shellcheck disable=SC2029
 ssh ${REMOTE_SERVER} "docker-compose -f ${DOCKER_COMPOSE_FILE_PATH} --env-file=${ENV_FILE} up -d --build"
 ssh ${REMOTE_SERVER} "docker ps"
+
+
+### Manual execute and build script ###
+ssh lightsail "docker-compose -f project/afreeca-scraper/docker-compose.yml down"
+ssh lightsail "rm -rf project/afreeca-scraper"
+rsync -av ./ lightsail:~/project/afreeca-scraper
+
+# Dev
+ssh lightsail "docker-compose -f project/afreeca-scraper/docker-compose.yml --env-file=project/afreeca-scraper/.env up -d --build"
+# Prod
+ssh lightsail "docker-compose -f project/afreeca-scraper/docker-compose.yml --env-file=project/afreeca-scraper/.env.prod up -d --build"
